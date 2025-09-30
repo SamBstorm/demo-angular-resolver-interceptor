7fc8db9dcac1dcc7fd0ba6b34348661329a8b80a
Samuel Legrain <samuel.legrain@bstorm.be>
2025-09-30 12:30:50 +0200

    Ajout de la vue details

--- src/app/features/pages/books/books-details/books-details.ts
+++ src/app/features/pages/books/books-details/books-details.ts
@@ -1,11 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { IBook } from '../../../models/ibook';
+import { ActivatedRoute, RouterLink } from '@angular/router';
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-books-details',
-  imports: [],
+  imports: [DatePipe, RouterLink],
   templateUrl: './books-details.html',
   styleUrl: './books-details.scss'
 })
-export class BooksDetails {
+export class BooksDetails implements OnInit {
+  public book! : IBook;
+  private _actRoute : ActivatedRoute = inject(ActivatedRoute);
 
+  ngOnInit(): void {
+    this.book = this._actRoute.snapshot.data['bookData'];
+  }
 }
