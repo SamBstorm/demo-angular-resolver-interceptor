1edda138708f4fa762f5ea19e36d9b4c82db9ecd
Samuel Legrain <samuel.legrain@bstorm.be>
2025-09-30 11:42:56 +0200

    Avant resolver

--- src/app/features/pages/books/books-list/books-list.ts
+++ src/app/features/pages/books/books-list/books-list.ts
@@ -1,11 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { IBook } from '../../../models/ibook';
+import { BooksFakeApi } from '../../../services/books-fake-api';
+import { JsonPipe } from '@angular/common';
 
 @Component({
   selector: 'app-books-list',
-  imports: [],
+  imports: [JsonPipe],
   templateUrl: './books-list.html',
   styleUrl: './books-list.scss'
 })
-export class BooksList {
+export class BooksList implements OnInit {
+  public books! : IBook[];
+  private _bookService : BooksFakeApi = inject(BooksFakeApi);
 
+  ngOnInit(): void {
+    this._bookService.getAll().subscribe({
+      next : (datas) => this.books = datas,
+      error : (err) => console.error(err)
+    });
+  }
 }
---
+++ src/app/features/services/books-fake-api.ts
@@ -0,0 +1,17 @@
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from '../../../environments/environment';
+import { IBook } from '../models/ibook';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class BooksFakeApi {
+  private _http : HttpClient = inject(HttpClient);
+  private _apiUrlBooks : string = environment.apiUrl + 'books/';
+
+  public getAll() : Observable<IBook[]>{
+    return this._http.get<IBook[]>(this._apiUrlBooks);
+  }
+}
